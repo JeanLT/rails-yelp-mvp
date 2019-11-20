7b537f3b1889f1ac6a8238d2d08a217fa184a02c
Jean Le Tulzo <jean.letulzo@gmail.com>
2019-11-20 15:31:46 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '02839283928',
+    category:     'chinese'
+  },
+  {
+    name:         'La baguette',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07927492748',
+    category:     'french'
+  },
+  {
+    name:         'La meilleure pizz',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07927492748',
+    category:     'italian'
+  },
+  {
+    name:         'le top sushi',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07987998898',
+    category:     'japanese'
+  },
+  {
+    name:         'la bonne frite',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '098723978',
+    category:     'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
